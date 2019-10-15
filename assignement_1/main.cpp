003022ca1f5f04170caae2635a7fe6aacff95f42
Marc-Etienne Dartus <marcetienne.dartus@gmail.com>
2019-10-14 21:32:58 -0700

    modif

--- assignement_1/main.cpp
+++ assignement_1/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
 using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
+using std::vector;
 
 /**
  * * Function for exercise 1
@@ -15,15 +18,66 @@ void checkPolynomial(string polynomial)
     return;
 }
 
+string RemoveSpace(string polynomial)
+{
+    string newString = "";
+    for (int position = 0; position < polynomial.size(); position++)
+    {
+        if(polynomial[position] != ' ') newString += polynomial[position];
+    }
+    return newString;
+}
+
+vector<string> Split(string polynomial,char splitChar)
+{
+    vector<string> splitVector;
+    string splitString = "";
+    for (int position = 0; position < polynomial.size(); position++)
+    {
+        if(polynomial[position] != ' '){
+            if(polynomial[position] != splitChar) splitString += polynomial[position];
+            else {
+                splitVector.push_back(splitString);
+                splitString = "";
+            }
+        }
+    }
+    return splitVector;
+}
+
+
+bool findNativeError(string polynomial,vector<char> exception)
+{
+    bool hasError = false;
+    int position = 0;
+    while(!hasError && position < polynomial.size()){
+        int exceptionIndex = 0;
+        while(!hasError && exceptionIndex < exception.size()) {
+            hasError = polynomial[position] == exception[exceptionIndex];
+            exceptionIndex++;
+        }
+        position++;
+    }
+    return hasError;
+}
+
 /**
  * * First exercise of the assignement
  */
 void Exercise1()
 {
-    cout << "Enter a polynomial: " << endl;
-    string polynomial;
-    cin >> polynomial;
-    checkPolynomial(polynomial);
+    // cout << "Enter a polynomial: " << endl;
+    string polynomial = "2*n + 4.54* n^5 +4 +5*n";
+    // cin >> polynomial;e
+    vector<string> plop = Split(polynomial,'+');
+    for (size_t i = 0; i < plop.size(); i++)
+    {
+       cout << plop[i] << endl;
+    }
+
+    cout << findNativeError(polynomial,{'(',')','-'});
+    
+    cout << system("pause");
 }
 
 /**
