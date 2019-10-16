ae2391f020202e5c37fe06cc0c90481334c26abc
Exorth <nico.caill@live.fr>
2019-10-15 18:48:25 -0700

    Exercise3

--- assignement_2/DoubleStack.h
+++ assignement_2/DoubleStack.h
@@ -1,30 +1,35 @@
-#include <iostream>
-#include <string>
-#include <vector>
-
-using std::cin;
-using std::cout;
-using std::endl;
-using std::string;
-using std::vector;
+#ifndef DOUBLESTACK_H
+#define DOUBLESTACK_H
 
 template <typename T>
 class DoubleStack
 {
 private:
-    T data[];
+    T* data;
+    int firstTop;
+    int secondTop;
     int size;
+    int fristSize;
+    int secondSize;
+    void doubleLength();
 
 public:
     DoubleStack();
     ~DoubleStack();
+
     // member methods
     void pushFirst(T);
     void pushSecond(T);
     void popFirst();
     void popSecond();
+
+    // optionnal
     const T peekFirst() const;
     const T peekSecond() const;
     const bool emptyFirst() const;
     const bool emptySecond() const;
+
+    const void print() const;
 };
+
+#endif
