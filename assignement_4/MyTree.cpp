396aa4dcdb658ceb537775d9e444aae4cb8e8dc6
Alexandre Zajac <zajac.alexandre@gmail.com>
2019-10-31 01:24:42 -0700

    Assignement 4 first commit

---
+++ assignement_4/MyTree.cpp
@@ -0,0 +1,13 @@
+#include "MyTree.h"
+
+MyTree::MyTree() {}
+
+MyTree::~MyTree() {}
+
+void MyTree::insert (int x ,string s) {}
+
+void MyTree::preorder() const {}
+
+BinaryNode* MyTree::findMax() const { return NULL; }
+
+void MyTree::makeBST() {}
---
+++ assignement_4/test.cpp
@@ -0,0 +1,10 @@
+#include "MyTree.h"
+
+//Testing (you must implement yours for the perfomance studies)
+main() {
+
+  MyTree t;
+  //....
+  
+
+}
