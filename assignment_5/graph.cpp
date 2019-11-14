68b82bab83c24c97685de3a32dc682ab93f4537c
Alexandre Zajac <zajac.alexandre@gmail.com>
2019-11-14 09:56:29 -0800

    Ass5

---
+++ assignment_5/graph.cpp
@@ -0,0 +1,92 @@
+#include "graph.h"
+
+
+ vector<string>* Node::neighbors() {
+   vector<string>* vec = new vector<string>();
+   vector<Edge*>::iterator it;
+   for(it=adjacents->begin();it!=adjacents->end();it++) {
+     vec->push_back((*it)->getNode()->getPayload());
+   }
+   return vec;
+ }
+
+
+ void Graph::addNode(string name) {
+    Node* node = new Node(name);
+    graph->insert(pair<string,Node*>(name,node));
+ }
+
+ void Graph::addEdge(string k1, string k2, double w=1.0) {
+   map<string,Node* >::iterator it1,it2;
+   it1 = graph->find(k1);
+   it2 = graph->find(k2);
+
+   //Check both nodes exists using the map
+   if (it1 != graph->end() && it2 != graph->end()) {
+      Node* n1 = (*it1).second;
+      Node* n2 = (*it2).second;
+      //Create the edge and add it to the adjacents
+      Edge* e = new Edge(n2,w);
+      n1->addEdge(e);
+      //Undirected graph: we add the reverse or back edge
+      if (!directed) {
+        Edge* be = new Edge(n1,w);
+        n2->addEdge(be);
+      }
+   }
+ }
+
+
+void Graph::printGraph() {
+   //Iterate each node
+  map<string,Node* >::iterator it;
+  for (it=graph->begin(); it != graph->end();it++) {
+    Node* n = (*it).second;
+    cout << n->getPayload() << "[";
+    
+    //Iterate the neighboors
+    vector<string>* neigh  = n->neighbors(); 
+    vector<string>::iterator itn;
+    for(itn=neigh->begin();itn!=neigh->end();itn++) {
+       cout << (*itn) << " ";
+    }
+    cout << "]" << endl;
+  }
+}
+
+bool Graph::IsThereTripletClique(){
+
+}
+
+ double Graph:: GetMinDistance(string city1,string city2){
+
+ }
+
+ bool Graph::isGraphConnected() {
+   
+ }
+
+ int Graph::longestSimplePath() {
+
+ }
+
+
+
+int main() {
+  cout << "Graph Example 2.0\n";
+  Graph g(false);
+  
+  g.addNode("a");
+  g.addNode("b");
+  g.addNode("c");
+
+  g.addEdge("a","b",10);
+  g.addEdge("b","c",5);
+  g.addEdge("c","a",9);
+
+
+  g.printGraph();
+
+}
+
+
